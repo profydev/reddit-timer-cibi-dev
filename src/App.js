15d5848d5e12b499714fab4213389fde86b37e67
devvvd <63036772+devvvd@users.noreply.github.com>
2021-08-27 12:00:04 +0530

    Create app skeleton (#2)
    * Create app skeleton
    
    - install fonts (Bitter and Montserrat)
    - reset default browser styles (for example with normalize.css)
    - add global styles
    - add router with routes / and /search
    - setup folder structure (optional, can be done in later tasks as well)
    
    * Change app title and meta description

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import HomePage from './pages/HomePage/HomePage';
+import SearchPage from './pages/SearchPage/SearchPage';
+import GlobalStyles from './GlobalStyles';
+
+import './normalize.css';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <BrowserRouter>
+      <GlobalStyles />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/search" component={SearchPage} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/GlobalStyles.js
@@ -0,0 +1,165 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+
+    /* Bitter */
+    @font-face {
+        font-family: 'Bitter';
+        src: local('Bitter'), url('./fonts/Bitter/Bitter-VariableFont_wght.ttf');
+        font-style: normal;
+    }
+
+    /* Bitter Italic */
+    @font-face {
+        font-family: 'Bitter';
+        src: local('Bitter'), url('./fonts/Bitter/Bitter-Italic-VariableFont_wght.ttf');
+        font-style: italic;
+    }
+
+    /* Montserrat Thin */
+    @font-face {
+        font-family: 'Montserrat';
+        src: local('Montserrat'), url('./fonts/Montserrat/Montserrat-Thin.ttf');
+        font-weight: 100;
+        font-style: normal;
+    }
+
+    /* Montserrat Thin Italic */
+    @font-face {
+        font-family: 'Montserrat';
+        src: local('Montserrat'), url('./fonts/Montserrat/Montserrat-ThinItalic.ttf');
+        font-weight: 100;
+        font-style: thin;
+    }
+
+    /* Montserrat Extra Light */
+    @font-face {
+        font-family: 'Montserrat';
+        src: local('Montserrat'), url('./fonts/Montserrat/Montserrat-ExtraLight.ttf');
+        font-weight: 200;
+        font-style: normal;
+    }
+
+    /* Montserrat Extra Light Italic */
+    @font-face {
+        font-family: 'Montserrat';
+        src: local('Montserrat'), url('./fonts/Montserrat/Montserrat-ExtraLightItalic.ttf');
+        font-weight: 200;
+        font-style: italic;
+    }
+
+    /* Montserrat Light */
+    @font-face {
+        font-family: 'Montserrat';
+        src: local('Montserrat'), url('./fonts/Montserrat/Montserrat-Light.ttf');
+        font-weight: 300;
+        font-style: normal;
+    }
+
+    /* Montserrat Light Italic */
+    @font-face {
+        font-family: 'Montserrat';
+        src: local('Montserrat'), url('./fonts/Montserrat/Montserrat-Light.ttf');
+        font-weight: 300;
+        font-style: normal;
+    }
+
+    /* Montserrat Regular */
+    @font-face {
+        font-family: 'Montserrat';
+        src: local('Montserrat'), url('./fonts/Montserrat/Montserrat-Regular.ttf');
+        font-weight: 400;
+        font-style: normal;
+    }
+
+    /* Montserrat Regular Italic */
+    @font-face {
+        font-family: 'Montserrat';
+        src: local('Montserrat'), url('./fonts/Montserrat/Montserrat-Italic.ttf');
+        font-weight: 400;
+        font-style: italic;
+    }
+
+    /* Montserrat Medium */
+    @font-face {
+        font-family: 'Montserrat';
+        src: local('Montserrat'), url('./fonts/Montserrat/Montserrat-Medium.ttf');
+        font-weight: 500;
+        font-style: normal;
+    }
+
+    /* Montserrat Medium Italic */
+    @font-face {
+        font-family: 'Montserrat';
+        src: local('Montserrat'), url('./fonts/Montserrat/Montserrat-MediumItalic.ttf');
+        font-weight: 500;
+        font-style: italic;
+    }
+
+    /* Montserrat Semi-Bold */
+    @font-face {
+        font-family: 'Montserrat';
+        src: local('Montserrat'), url('./fonts/Montserrat/Montserrat-SemiBold.ttf');
+        font-weight: 600;
+        font-style: normal;
+    }
+
+    /* Montserrat Semi-Bold Italic */
+    @font-face {
+        font-family: 'Montserrat';
+        src: local('Montserrat'), url('./fonts/Montserrat/Montserrat-SemiBoldItalic.ttf');
+        font-weight: 600;
+        font-style: italic;
+    }
+
+    /* Montserrat Bold */
+    @font-face {
+        font-family: 'Montserrat';
+        src: local('Montserrat'), url('./fonts/Montserrat/Montserrat-Bold.ttf');
+        font-weight: 700;
+        font-style: normal;
+    }
+
+    /* Montserrat Bold Italic */
+    @font-face {
+        font-family: 'Montserrat';
+        src: local('Montserrat'), url('./fonts/Montserrat/Montserrat-BoldItalic.ttf');
+        font-weight: 700;
+        font-style: italic;
+    }
+
+    /* Montserrat Extra-Bold */
+    @font-face {
+        font-family: 'Montserrat';
+        src: local('Montserrat'), url('./fonts/Montserrat/Montserrat-ExtraBold.ttf');
+        font-weight: 800;
+        font-style: normal;
+    }
+
+    /* Montserrat Extra-Bold Italic */
+    @font-face {
+        font-family: 'Montserrat';
+        src: local('Montserrat'), url('./fonts/Montserrat/Montserrat-ExtraBoldItalic.ttf');
+        font-weight: 800;
+        font-style: italic;
+    }
+
+    /* Montserrat Black */
+    @font-face {
+        font-family: 'Montserrat';
+        src: local('Montserrat'), url('./fonts/Montserrat/Montserrat-Black.ttf');
+        font-weight: 900;
+        font-style: normal;
+    }
+
+    /* Montserrat Black Italic */
+    @font-face {
+        font-family: 'Montserrat';
+        src: local('Montserrat'), url('./fonts/Montserrat/Montserrat-BlackItalic.ttf');
+        font-weight: 900;
+        font-style: italic;
+    }
+
+`;
+
+export default GlobalStyles;
