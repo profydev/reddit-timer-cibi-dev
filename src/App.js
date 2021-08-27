4b16b17d2768a796fd958d7c0e9bfe921d844b0c
devvvd <63036772+devvvd@users.noreply.github.com>
2021-08-27 12:33:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-
 import Header from './components/Header';
 import HomePage from './pages/HomePage/HomePage';
 import SearchPage from './pages/SearchPage/SearchPage';
